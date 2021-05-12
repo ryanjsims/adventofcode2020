29ee1ef748df92359ab9a9d2bb89b29241d142ef
Ryan Sims <ryanjsims@email.arizona.edu>
2021-05-12 13:48:50 -0700

    update template

--- template.cpp
+++ template.cpp
@@ -5,4 +5,12 @@ int main(){
     std::ifstream input;
     input.open("input.txt", std::ifstream::in);
 
+    std::string line;
+    std::getline(input, line);
+    while(input.good()){
+        
+        std::getline(input, line);
+    }
+
+    return 0;
 }
