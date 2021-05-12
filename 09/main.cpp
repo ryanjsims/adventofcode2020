774ff031796911230e2a9589969cf8a15e583fd8
Ryan Sims <ryanjsims@email.arizona.edu>
2021-05-12 11:11:19 -0700

    Solved 09 pt 2

--- 09/main.cpp
+++ 09/main.cpp
@@ -15,6 +15,23 @@ bool check_element(std::vector<uint64_t> data, int index){
     return false;
 }
 
+std::pair<int, int> find_range(std::vector<uint64_t> data, uint64_t target){
+    int start = 0;
+    while(start < data.size() && data[start] + data[start + 1] <= target){
+        int index = start + 2;
+        uint64_t sum = data[start] + data[start + 1];
+        while(sum < target){
+            sum += data[index];
+            index++;
+        }
+        if(sum == target){
+            return std::make_pair(start, index);
+        }
+        start++;
+    }
+    return std::make_pair(-1, -1);
+}
+
 int main(){
     std::ifstream input;
     input.open("input.txt", std::ifstream::in);
@@ -32,5 +49,19 @@ int main(){
         index++;
     }
     std::cout << "Element at index " << index << ": " << data[index] << " is not the sum of two elements in the preamble" << std::endl;
+    
+    std::pair<int, int> range = find_range(data, data[index]);
+    std::cout << "Elements in index range " << range.first << " to " << range.second - 1 << " sum to " << data[index] << std::endl;
+    uint64_t min = data[index], max = 0;
+    for(int i = range.first; i < range.second; i++){
+        if(data[i] < min){
+            min = data[i];
+        }
+        if(data[i] > max){
+            max = data[i];
+        }
+    }
+    std::cout << "The min and max of that range are " << min << " and " << max << std::endl;
+    std::cout << "They sum to " << min + max << std::endl;
     return 0;
 }
