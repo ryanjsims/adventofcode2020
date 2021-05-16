e384d76073e5279b01a5f8d8379fc61cd9ec6e2b
Ryan Sims <ryanjsims@email.arizona.edu>
2021-05-16 16:09:00 -0700

    Technically should work just super slow

--- 17/main.cpp
+++ 17/main.cpp
@@ -5,7 +5,7 @@
 #include <unordered_map>
 
 struct coordinate {
-    int x, y, z;
+    int x, y, z, w;
     int& operator[](int index) {
         if(index == 0)
             return x;
@@ -13,38 +13,61 @@ struct coordinate {
             return y;
         if(index == 2)
             return z;
+        if(index == 3)
+            return w;
         throw new std::out_of_range("Must be in range [0, 2]"); 
     }
+
+    inline std::size_t size(){
+        return 4;
+    }
+
     bool operator==(const coordinate &other) const {
-        return this->x == other.x && this->y == other.y && this->z == other.z;
+        return this->x == other.x && this->y == other.y && this->z == other.z && this->w == other.w;
     }
 
     /**
      * operator< - Strict compare, returns true iff all three coordinates are <= the other's
      * */
     bool operator<(const coordinate &other) const {
-        return this->x <= other.x && this->y <= other.y && this->z <= other.z;
+        return this->x <= other.x && this->y <= other.y && this->z <= other.z && this->w <= other.w;
     }
 
     /**
      * operator> - Strict compare, returns true iff all three coordinates are >= the other's
      * */
     bool operator>(const coordinate &other) const {
-        return this->x >= other.x && this->y >= other.y && this->z >= other.z;
+        return this->x >= other.x && this->y >= other.y && this->z >= other.z && this->w >= other.w;
     }
 
     /**
      * operator<= - Loose compare, returns true if any of the coordinates are <= the other's corresponding coordinates
      * */
     bool operator<=(const coordinate &other) const {
-        return this->x <= other.x || this->y <= other.y || this->z <= other.z;
+        return this->x <= other.x || this->y <= other.y || this->z <= other.z || this->w <= other.w;
     }
 
     /**
      * operator>= - Loose compare, returns true if any of the coordinates are >= the other's corresponding coordinates
      * */
     bool operator>=(const coordinate &other) const {
-        return this->x >= other.x || this->y >= other.y || this->z >= other.z;
+        return this->x >= other.x || this->y >= other.y || this->z >= other.z || this->w >= other.w;
+    }
+
+    coordinate minimize(coordinate other){
+        coordinate to_return;
+        for(int i = 0; i < to_return.size(); i++){
+            to_return[i] = std::min((*this)[i], other[i]);
+        }
+        return to_return;
+    }
+
+    coordinate maximize(coordinate other){
+        coordinate to_return;
+        for(int i = 0; i < to_return.size(); i++){
+            to_return[i] = std::max((*this)[i], other[i]);
+        }
+        return to_return;
     }
 };
 
@@ -52,34 +75,33 @@ namespace std {
     template <>
     struct hash<coordinate> {
         size_t operator()(const coordinate& k) const {
-            return (size_t)k.x ^ ((size_t)k.y) << 8 ^ ((size_t)k.z) << 16;
+            return (size_t)k.x ^ ((size_t)k.y) << 8 ^ ((size_t)k.z) << 16 ^ ((size_t)k.w) << 24;
         }
     };
 
     ostream& operator<<(ostream& os, const coordinate& coord){
         os << "(" << std::setw(2) << std::right << coord.x 
            << "," << std::setw(2) << std::right << coord.y 
-           << "," << std::setw(2) << std::right << coord.z << ")";
+           << "," << std::setw(2) << std::right << coord.z
+           << "," << std::setw(2) << std::right << coord.w << ")";
         return os;
     }
 }
 
-void print_coord(coordinate c){
-    std::cout << "(" << c.x << ", " << c.y << ", " << c.z << ")";
-}
-
 int neighbor_count(std::unordered_map<coordinate, bool> world, coordinate cell){
     int count = 0;
-    for(int z = cell.z - 1; z <= cell.z + 1; z++){
-        for(int y = cell.y - 1; y <= cell.y + 1; y++){
-            for(int x = cell.x - 1; x <= cell.x + 1; x++){
-                if(x == cell.x && y == cell.y && z == cell.z)
-                    continue;
-                if(world[{x, y, z}]){
-                    //std::cout << "Found neighbor at ";
-                    //print_coord({x, y, z});
-                    //std::cout << std::endl;
-                    count++;
+    for(int w = cell.w - 1; w <= cell.w + 1; w++){
+        for(int z = cell.z - 1; z <= cell.z + 1; z++){
+            for(int y = cell.y - 1; y <= cell.y + 1; y++){
+                for(int x = cell.x - 1; x <= cell.x + 1; x++){
+                    if(coordinate{x, y, z, w} == cell)
+                        continue;
+                    if(world[{x, y, z, w}]){
+                        //std::cout << "Found neighbor at ";
+                        //print_coord({x, y, z});
+                        //std::cout << std::endl;
+                        count++;
+                    }
                 }
             }
         }
@@ -87,7 +109,7 @@ int neighbor_count(std::unordered_map<coordinate, bool> world, coordinate cell){
     return count;
 }
 
-void draw_plane(std::unordered_map<coordinate, bool> world, int z, std::pair<int, int> x, std::pair<int, int> y){
+void draw_plane(std::unordered_map<coordinate, bool> world, int w, int z, std::pair<int, int> x, std::pair<int, int> y){
     std::cout << "   ";
     for(int i = x.first; i <= x.second; i++){
         std::cout << std::setw(3) << std::right << i;
@@ -98,9 +120,9 @@ void draw_plane(std::unordered_map<coordinate, bool> world, int z, std::pair<int
         std::cout << std::setw(3) << std::right << j;
         for(int i = x.first; i <= x.second; i++){
             if(world[{i, j, z}]){
-                std::cout << "\033[1;32m" << std::setw(3) << std::right << neighbor_count(world, {i, j, z}) << "\033[0m";
+                std::cout << "\033[1;32m" << std::setw(3) << std::right << neighbor_count(world, {i, j, z, w}) << "\033[0m";
             } else {
-                std::cout << "\033[1;31m" << std::setw(3) << std::right << neighbor_count(world, {i, j, z}) << "\033[0m";
+                std::cout << "\033[1;31m" << std::setw(3) << std::right << neighbor_count(world, {i, j, z, w}) << "\033[0m";
             }
         }
         std::cout << std::endl;
@@ -108,36 +130,47 @@ void draw_plane(std::unordered_map<coordinate, bool> world, int z, std::pair<int
     std::cout << std::endl << std::endl;
 }
 
-void draw_cube(std::unordered_map<coordinate, bool> world, std::pair<coordinate, coordinate> AABB, int offset = 0){
+void draw_cube(std::unordered_map<coordinate, bool> world, int w, std::pair<coordinate, coordinate> AABB, int offset = 0){
     for(int z = AABB.first.z - offset; z <= AABB.second.z + offset; z++){
-        std::cout << "z: " << z << std::endl;
-        draw_plane(world, z, std::make_pair(AABB.first.x - offset, AABB.second.x + offset), 
+        std::cout << "(z, w): (" << z << ", " << w << ")" << std::endl;
+        draw_plane(world, w, z, std::make_pair(AABB.first.x - offset, AABB.second.x + offset), 
                              std::make_pair(AABB.first.y - offset, AABB.second.y + offset));
     }
 }
 
-int find_min_plane(std::unordered_map<coordinate, bool> world,
+void draw_hypercube(std::unordered_map<coordinate, bool> world, std::pair<coordinate, coordinate> AABB, int offset = 0){
+    for(int w = AABB.first.w - offset; w <= AABB.second.w + offset; w++){
+        draw_cube(world, w, AABB, offset);
+    }
+}
+
+int find_min_cube(std::unordered_map<coordinate, bool> world,
                     std::pair<int, int> search_bounds,
                     std::pair<int, int> plane1_bounds,
                     std::pair<int, int> plane2_bounds,
-                    std::array<int, 3> index,
+                    std::pair<int, int> plane3_bounds,
+                    std::array<int, 4> index,
                     int* cells = nullptr){
     int directionz = (search_bounds.second - search_bounds.first) / std::abs(search_bounds.second - search_bounds.first);
     int directiony = (plane1_bounds.second - plane1_bounds.first) / std::abs(plane1_bounds.second - plane1_bounds.first);
     int directionx = (plane2_bounds.second - plane2_bounds.first) / std::abs(plane2_bounds.second - plane2_bounds.first);
+    int directionw = (plane3_bounds.second - plane3_bounds.first) / std::abs(plane3_bounds.second - plane3_bounds.first);
     if(cells != nullptr)
         *cells = 0;
     for(int z = search_bounds.first; z != search_bounds.second + directionz; z+=directionz){
         for(int y = plane1_bounds.first; y != plane1_bounds.second + directiony; y+=directiony){
             for(int x = plane2_bounds.first; x != plane2_bounds.second + directionx; x+=directionx){
-                coordinate loc;
-                loc[index[0]] = z;
-                loc[index[1]] = y;
-                loc[index[2]] = x;
-                if(cells != nullptr)
-                    (*cells)++;
-                if(world[loc]){
-                    return z;
+                for(int w = plane3_bounds.first; w != plane3_bounds.second + directionw; w+=directionw){
+                    coordinate loc;
+                    loc[index[0]] = z;
+                    loc[index[1]] = y;
+                    loc[index[2]] = x;
+                    loc[index[3]] = w;
+                    if(cells != nullptr)
+                        (*cells)++;
+                    if(world[loc]){
+                        return z;
+                    }
                 }
             }
         }
@@ -146,131 +179,103 @@ int find_min_plane(std::unordered_map<coordinate, bool> world,
 }
 
 std::pair<coordinate, coordinate> get_minimum_AABB(std::unordered_map<coordinate, bool> world, std::pair<coordinate, coordinate> prevAABB){
-    coordinate min = prevAABB.second;
-    coordinate max = prevAABB.first;
-    if(world.size() < 250){
-        for(auto it = world.begin(); it != world.end(); it++){
-            if(!it->second)
-                continue;
-            if(it->first.x < min.x){
-                min.x = it->first.x;
-            }
-            if(it->first.x > max.x){
-                max.x = it->first.x;
-            }
-            if(it->first.y < min.y){
-                min.y = it->first.y;
-            }
-            if(it->first.y > max.y){
-                max.y = it->first.y;
-            }
-            if(it->first.z < min.z){
-                min.z = it->first.z;
-            }
-            if(it->first.z > max.z){
-                max.z = it->first.z;
-            }
+    coordinate min = {prevAABB.second.x, prevAABB.second.y, prevAABB.second.z, prevAABB.second.w};
+    coordinate max = {prevAABB.first.x, prevAABB.first.y, prevAABB.first.z, prevAABB.first.w};
+    for(auto it = world.begin(); it != world.end(); it++){
+        if(!it->second)
+            continue;
+        if(it->first.x < min.x){
+            min.x = it->first.x;
         }
-    } else {
-        int total_cells = 0, cells;
-        min.z = find_min_plane(world, {prevAABB.first.z - 1, prevAABB.second.z}, 
-                                    {prevAABB.first.y - 1, prevAABB.second.y + 1}, 
-                                    {prevAABB.first.x - 1, prevAABB.second.x + 1},
-                                    std::array<int, 3> {2, 1, 0}, &cells);
-        total_cells += cells;
-        min.y = find_min_plane(world, {prevAABB.first.y - 1, prevAABB.second.y}, 
-                                    {prevAABB.first.z - 1, prevAABB.second.z + 1}, 
-                                    {prevAABB.first.x - 1, prevAABB.second.x + 1},
-                                    std::array<int, 3> {1, 2, 0}, &cells);
-        total_cells += cells;
-        min.x = find_min_plane(world, {prevAABB.first.x - 1, prevAABB.second.x}, 
-                                    {prevAABB.first.y - 1, prevAABB.second.y + 1}, 
-                                    {prevAABB.first.z - 1, prevAABB.second.z + 1},
-                                    std::array<int, 3> {0, 1, 2}, &cells);
-        total_cells += cells;
-        max.z = find_min_plane(world, {prevAABB.second.z + 1, prevAABB.first.z}, 
-                                    {prevAABB.first.y - 1, prevAABB.second.y + 1}, 
-                                    {prevAABB.first.x - 1, prevAABB.second.x + 1},
-                                    std::array<int, 3> {2, 1, 0}, &cells);
-        total_cells += cells;
-        max.y = find_min_plane(world, {prevAABB.second.y + 1, prevAABB.first.y}, 
-                                    {prevAABB.first.z - 1, prevAABB.second.z + 1}, 
-                                    {prevAABB.first.x - 1, prevAABB.second.x + 1},
-                                    std::array<int, 3> {1, 2, 0}, &cells);
-        total_cells += cells;
-        max.x = find_min_plane(world, {prevAABB.second.x + 1, prevAABB.first.x}, 
-                                    {prevAABB.first.y - 1, prevAABB.second.y + 1}, 
-                                    {prevAABB.first.z - 1, prevAABB.second.z + 1},
-                                    std::array<int, 3> {0, 1, 2}, &cells);
-        total_cells += cells;
-        std::cout << "Total cells: " << total_cells << std::endl;
-        std::cout << "World size:  " << world.size() << std::endl;
+        if(it->first.x > max.x){
+            max.x = it->first.x;
+        }
+        if(it->first.y < min.y){
+            min.y = it->first.y;
+        }
+        if(it->first.y > max.y){
+            max.y = it->first.y;
+        }
+        if(it->first.z < min.z){
+            min.z = it->first.z;
+        }
+        if(it->first.z > max.z){
+            max.z = it->first.z;
+        }
+        if(it->first.w < min.w){
+            min.w = it->first.w;
+        }
+        if(it->first.w > max.w){
+            max.w = it->first.w;
+        }
     }
     return std::make_pair(min, max);
 }
 
 int main(){
     std::ifstream input;
-    input.open("input.txt", std::ifstream::in);
+    input.open("smallinput.txt", std::ifstream::in);
 
     std::unordered_map<coordinate, bool> world1, world2;
     std::string line;
-    std::pair<coordinate, coordinate> AABB = std::make_pair(coordinate{0, 0, 0}, coordinate{0, 0, 0}), AABB2 = std::make_pair(coordinate{0, 0, 0}, coordinate{0, 0, 0});
-    int x = 0, y = 0, z = 0;
+    std::pair<coordinate, coordinate> AABB = std::make_pair(coordinate{0, 0, 0, 0}, coordinate{0, 0, 0, 0});
+    int x = 0, y = 0, z = 0, w = 0;
     while(input.good()){
         std::getline(input, line);
+        std::cout << line << std::endl;
         if(line == "")
             break;
         for(int x = 0; x < line.size(); x++){
+            coordinate curr = {x, y, z, w};
             switch(line[x]){
             case '#':
-                if(coordinate{x, y, z} < AABB.first){
-                    AABB.first = coordinate{x, y, z};
-                    AABB2.first = AABB.first;
-                }
-                if(coordinate{x, y, z} > AABB.second){
-                    AABB.second = coordinate{x, y, z};
-                    AABB2.second = AABB.second;
-                }
-                world1[{x, y, z}] = true;
-                world2[{x, y, z}] = true;
+                AABB.first = AABB.first.minimize(curr);
+                AABB.second = AABB.second.maximize(curr);
+                world1[curr] = true;
+                world2[curr] = true;
                 break;
             case '.':
-                world1[{x, y, z}] = false;
-                world2[{x, y, z}] = false;
+                world1[curr] = false;
+                world2[curr] = false;
                 break;
             }
         }
         y++;
     }
+    std::cout << AABB.first << std::endl;
+    std::cout << AABB.second << std::endl;
+    std::cin.get();
     
     std::unordered_map<coordinate, bool>* world = &world1, *next_world = &world2;
     for(int i = 0; i < 6; i++){
-        //std::cout << "Iteration " << i << std::endl;
+        std::cout << "Iteration " << i << std::endl;
+        draw_hypercube(*world, AABB);
         AABB = get_minimum_AABB(*world, AABB);
-        for(z = AABB.first.z - 1; z <= AABB.second.z + 1; z++){
-            for(y = AABB.first.y - 1; y <= AABB.second.y + 1; y++){
-                for(x = AABB.first.x - 1; x <= AABB.second.x + 1; x++){
-                    //std::cout << coordinate{x, y, z} << ": ";
-                    switch(neighbor_count(*world, {x, y, z})){
-                    case 2:
-                        //std::cout << "maintaining";
-                        (*next_world)[{x, y, z}] = (*world)[{x, y, z}];
-                        break;
-                    case 3:
-                        (*next_world)[{x, y, z}] = true;
-                        if(!(*world)[{x, y, z}]){
-                            //std::cout << "activating";
+        for(w = AABB.first.w - 1; w <= AABB.second.w + 1; w++){
+            for(z = AABB.first.z - 1; z <= AABB.second.z + 1; z++){
+                for(y = AABB.first.y - 1; y <= AABB.second.y + 1; y++){
+                    for(x = AABB.first.x - 1; x <= AABB.second.x + 1; x++){
+                        //std::cout << coordinate{x, y, z} << ": ";
+                        switch(neighbor_count(*world, {x, y, z, w})){
+                        case 2:
+                            //std::cout << "maintaining";
+                            (*next_world)[{x, y, z, w}] = (*world)[{x, y, z, w}];
+                            break;
+                        case 3:
+                            (*next_world)[{x, y, z, w}] = true;
+                            if(!(*world)[{x, y, z, w}]){
+                                //std::cout << "activating";
+                            }
+                            break;
+                        default:
+                            (*next_world)[{x, y, z, w}] = false;
+                            if((*world)[{x, y, z, w}]){
+                                //std::cout << "deactivating";
+                            }
+                            break;
                         }
-                        break;
-                    default:
-                        (*next_world)[{x, y, z}] = false;
-                        if((*world)[{x, y, z}]){
-                            //std::cout << "deactivating";
-                        }
-                        break;
+                        //std::cout << std::endl;
                     }
-                    //std::cout << std::endl;
                 }
             }
         }
@@ -281,8 +286,8 @@ int main(){
         //std::cin.get();
     }
     AABB = get_minimum_AABB(*world, AABB);
-    //std::cout << "Iteration " << 6 << std::endl;
-    //draw_cube(*world, AABB);
+    std::cout << "Iteration " << 6 << std::endl;
+    draw_hypercube(*world, AABB);
     std::cout << AABB.first << std::endl;
     std::cout << AABB.second << std::endl;
     int active = 0;
