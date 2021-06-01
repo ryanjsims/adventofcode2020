154f2794109ba6e5538aceb3c0820cf629e41d80
Ryan Sims <ryanjsims@email.arizona.edu>
2021-06-01 16:51:14 -0700

    Updated template

--- template.cpp
+++ template.cpp
@@ -10,10 +10,9 @@ int main(int argc, char** argv){
     input.open(filename, std::ifstream::in);
 
     std::string line;
-    std::getline(input, line);
     while(input.good()){
-        
         std::getline(input, line);
+        
     }
 
     return 0;
